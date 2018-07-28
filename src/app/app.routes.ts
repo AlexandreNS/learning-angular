f5ef316793ed2da8953bbb69eeb947f4d447461c
Alexandre <alexandregaming2015@hotmail.com>
2018-07-28 19:04:27 -0300

    Colocando Serviço de Restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from "app/home/home.component";
 import { HeaderComponent } from "app/header/header.component";
+import { AboutComponent } from "app/about/about.component";
+import { RestaurantsComponent } from "app/restaurants/restaurants.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'about', component: HeaderComponent}
+    { path: 'restaurants', component: RestaurantsComponent},
+    { path: 'about', component: AboutComponent}
 ];
